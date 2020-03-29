45578e1470f5d3a21d20ecbf89b825383be3fcb9
“eduardosantosgit” <eduardosantos058@gmail.com>
2020-03-29 16:24:04 -0300

    add querystring redirect

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 // load the things we need
 var express = require('express');
 var app = express();
-var mysql 		= require('mysql');
-var bodyParser  = require('body-parser');
+var mysql = require('mysql');
+var bodyParser = require('body-parser');
 
-var connection = mysql.createConnection({
-    host    : 'localhost',
-    user    : 'root',
-    password  : 'Br4s1l',
-    database  : 'teste'
-  });
+// var connection = mysql.createConnection({
+//     host    : 'localhost',
+//     user    : 'root',
+//     password  : '1234',
+//     database  : 'teste'
+//   });
 
-connection.connect(function(err) {
-	if (err) throw err;
-	console.log("Connected!");
-});
+// connection.connect(function(err) {
+// 	if (err) throw err;
+// 	console.log("Connected!");
+// });
 app.use(express.static('public'));
 app.use(express.static(__dirname + '/public'));
 
@@ -24,50 +24,67 @@ app.set('view engine', 'ejs');
 // use res.render to load up an ejs view file
 
 // index page 
-app.get('/', function(req, res) {
-	var paises='';
-	connection.query('select distinct country from svod ;', (err,rows) => {
-		if(err) throw err;
+app.get('/', function (req, res) {
+
+	let queryPaises = 'select distinct country from svod';
+	let queryNet = 'select network ,sum(case when quarter_year = ' + '"q12019"' + 'then export_data else 0 end) as q1,sum(case when quarter_year = ' + '"q22019"' + 'then export_data else 0 end) as q2,sum(case when quarter_year = ' + '"q32019"' + 'then export_data else 0 end) as q3 from svod where kpi_client = "SVOD and streaming service subscribers"group by network order by q1 desc;';
+	let queryQuarters = 'select Quarter_Year, sum(Export_data) as TotalQ from svod group by Quarter_Year;';
+
+	if (req.query.country !== undefined) {
+		queryPaises += "where country = " + req.query.country;
+		queryQuarters += "where country = " + req.query.country;
+	}
+
+	let paises;
+	connection.query(queryPaises, (err, rows) => {
+		if (err) throw err;
 		paises = rows;
 	});
-	var listnetwork ='';
-	connection.query('select network ,sum(case when quarter_year = '+'"q12019"'+ 'then export_data else 0 end) as q1,sum(case when quarter_year = ' + '"q22019"' + 'then export_data else 0 end) as q2,sum(case when quarter_year = '+'"q32019"' +'then export_data else 0 end) as q3 from svod where kpi_client = "SVOD and streaming service subscribers"group by network order by q1 desc;',
-	(err,rows) => {
-		if(err) throw err;
+
+	let listnetwork;
+	connection.query(queryNet, (err, rows) => {
+		if (err) throw err;
 		listnetwork = rows;
 	});
-	
-	connection.query('select Quarter_Year, sum(Export_data) as TotalQ from svod group by Quarter_Year;', (err,rows) => {
-		if(err) throw err;
-		var quarters= rows;
-		res.render('pages/index',{quarters:quarters,paises:paises,listnetwork:listnetwork});
-	  });
-	  //select * from jardim where idUsuario = ?', [idUsuario]
+
+	let quarters;
+	connection.query(queryQuarters, (err, rows) => {
+		if (err) throw err;
+		quarters = rows;
+
+		res.render('pages/index', { quarters: quarters, paises: paises, listnetwork: listnetwork });
+	});
+	//select * from jardim where idUsuario = ?', [idUsuario]
 });
 
-function getComboA(selectObject) {
-	var value = selectObject.value;  
-
-	connection.query('select distinct country from svod where country = ?',[value], 
-	(err,rows) => {
-		if(err) throw err;
-		paises = rows;
-	});
-
-	connection.query('select Quarter_Year, sum(Export_data) as TotalQ from svod group by Quarter_Year where country = ? ',[value], 
-	(err,rows) => {
-		if(err) throw err;
-		var analise= rows;
-		res.render('pages/index',{quarters:quarters,paises:paises});
-	  });
-	  console.log(value);
-  };
+app.get('/plataforms', function (req, res) {
+	res.render('pages/plataforms');
+});
 
 
 // about page 
-app.get('/plataforms', function(req, res) {
+app.get('/', function (req, res) {
 	res.render('pages/plataforms');
 });
 
 app.listen(3000);
 console.log('3000 is the magic port');
+
+// let paises = [
+// 	{ country: "Brasil" },
+// 	{ country: "Canada" },
+// 	{ country: "USA" }
+// ];
+
+// let quarters = [
+// 	{ Quarter_Year: "q32018", TotalQ: 84974784 },
+// 	{ Quarter_Year: "q32019", TotalQ: 84974784 },
+// 	{ Quarter_Year: "q32020", TotalQ: 84974784 }
+// ];
+
+// let listnetwork = [
+// 	{ network: "netflix", q1: 2828282, q2: 559859895, q3: 859598598 },
+// 	{ network: "claro", q1: 2828282, q2: 559859895, q3: 859598598 },
+// 	{ network: "hbo", q1: 2828282, q2: 559859895, q3: 859598598 },
+// 	{ network: "fox", q1: 2828282, q2: 559859895, q3: 859598598 }
+// ];
